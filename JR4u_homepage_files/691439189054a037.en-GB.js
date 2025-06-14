8f9ed4e3df259d46fd1f0ec9d22f843ccac4181d
Jan Ruzicka <honza@janruzicka.cz>
2025-06-14 21:31:58 +0200

    Delete JR4u_homepage_files directory

--- JR4u_homepage_files/691439189054a037.en-GB.js
+++
@@ -1,2 +0,0 @@
-window['cmsg']['assets'] = window['cmsg']['assets'] || {};
-window['cmsg']['assets']["en-GB"] = Object.assign(window['cmsg']['assets']["en-GB"] || {}, {"23019":{"js":["07a853010f4aa0c8.strings.js"],"css":[]},"72719":{"js":["a85507a1294f24cc.strings.js"],"css":[]}});
